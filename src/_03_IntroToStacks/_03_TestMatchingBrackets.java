2269aae9baf8d8b6a788b3779e739bd258f760c6
League <league@WTS9.local>
2018-11-29 18:25:21 -0800

    w

--- src/_03_IntroToStacks/_03_TestMatchingBrackets.java
+++ src/_03_IntroToStacks/_03_TestMatchingBrackets.java
@@ -5,6 +5,8 @@ import static org.junit.Assert.assertTrue;
 
 import java.util.Stack;
 
+import javax.xml.stream.events.Characters;
+
 import org.junit.Test;
 
 public class _03_TestMatchingBrackets {
@@ -18,9 +20,28 @@ public class _03_TestMatchingBrackets {
 		assertFalse(doBracketsMatch("}{"));
 	}
 
-	// USE A STACK TO COMPLETE THE METHOD FOR CHECKING IF EVERY OPENING BRACKET HAS A MATCHING CLOSING BRACKET
+	// USE A STACK TO COMPLETE THE METHOD FOR CHECKING IF EVERY OPENING BRACKET HAS
+	// A MATCHING CLOSING BRACKET
 	private boolean doBracketsMatch(String b) {
-		
+		Stack<Character> brackets = new Stack<Character>();
+		for (int i = 0; i < b.length(); i++) {
+			Character BracketChar = b.charAt(i);
+
+			if (BracketChar == '{') {
+				brackets.push(BracketChar);
+			}
+			if (BracketChar == '}') {
+				if (!brackets.isEmpty()) {
+					brackets.pop();
+				} else {
+					return false;
+				}
+
+			}
+		}
+if(brackets.isEmpty()) {
+	return true;
+}
 		return false;
 	}
 
--- src/_04_HangMan/HangMan.java
+++ src/_04_HangMan/HangMan.java
@@ -1,7 +1,17 @@
 package _04_HangMan;
 
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
 public class HangMan{
-	
+	JFrame frame = new JFrame();
+	JPanel panel= new JPanel();
+	JLabel label=new JLabel();
+	public HangMan() {
+	frame.add(panel);
+	panel.add(label);
+	}
 	
 	
 }
