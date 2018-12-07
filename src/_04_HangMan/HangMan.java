e5ab99b0cfb54929092de50efe290b4a037f1d20
League <league@WTS9.attlocal.net>
2018-12-06 18:26:06 -0800

    started hangman

--- src/_04_HangMan/HangMan.java
+++ src/_04_HangMan/HangMan.java
@@ -1,17 +1,40 @@
 package _04_HangMan;
 
+import java.util.Stack;
+
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
-public class HangMan{
+public class HangMan {
+	Stack<String> words = new Stack<String>();
 	JFrame frame = new JFrame();
-	JPanel panel= new JPanel();
-	JLabel label=new JLabel();
-	public HangMan() {
-	frame.add(panel);
-	panel.add(label);
+	JPanel panel = new JPanel();
+	JLabel label = new JLabel();
+
+	public static void main(String[] args) {
+		HangMan hangman = new HangMan();
 	}
-	
-	
+
+	public HangMan() {
+		String amountOfLetters = JOptionPane.showInputDialog("Please type in a number below.");
+		int numwords = Integer.parseInt(amountOfLetters);
+		for (int i = 0; i < numwords; i++) {
+			String dictionary = Utilities.readRandomLineFromFile("dictionary.txt");
+			if (!words.contains(dictionary)) {
+
+				words.push(dictionary);
+			}
+
+		}
+		frame.add(panel);
+		panel.add(label);
+		frame.pack();
+		frame.setVisible(true);
+		String gameWord=words.pop();
+		label.setText(gameWord);
+		
+		
+	}
 }
