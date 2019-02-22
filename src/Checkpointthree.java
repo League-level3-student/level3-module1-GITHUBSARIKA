47942da0f9efdd3f97f084e7317f45fe4443be99
GITHUBSARIKA <league@WTS-IM-17.attlocal.net>
2019-02-21 18:27:40 -0800

    d

--- src/Checkpointthree.java
+++ src/Checkpointthree.java
@@ -1,18 +1,17 @@
 import java.util.HashMap;
 
 public class Checkpointthree {
-public static void main(String[] args) {
-	HashMap<String, String> words=new HashMap<String, String>();
+	public static void main(String[] args) {
+		HashMap<String, String> words = new HashMap<String, String>();
 
-		words.put("hi","ih");
-		words.put("bye","eyb");
-		words.put("what","tahw");
-		words.put("hello","olleh");
-		words.put("taco","ocat");
+		words.put("hi", "ih");
+		words.put("bye", "eyb");
+		words.put("what", "tahw");
+		words.put("hello", "olleh");
+		words.put("taco", "ocat");
+		String list="";
+		for(int i:words.keySet())
 		System.out.println(words);
-		
-	
-	
-		
-}
+
+	}
 }
